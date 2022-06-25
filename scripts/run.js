2e601c333ed87855d6b4cdcd332de5015a993658
junkei <junkei@junkeinoMacBook-Pro.local>
2022-06-25 16:50:43 +0900

    section-2-Lesson-3

---
+++ scripts/run.js
@@ -0,0 +1,36 @@
+// run.js
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    const wavePortal = await waveContract.deployed();
+
+    console.log("Contract deployed to:", wavePortal.address);
+    console.log("Contract deployed by:", owner.address);
+    console.log("Contract randomPerson address:", randomPerson.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
